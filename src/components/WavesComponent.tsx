48e4fe290ebabb135cdba544ad6ad2936f9e7b53
Katalyst33 <>
2022-03-11 21:30:58 +0000

    skeleton

---
+++ src/components/WavesComponent.tsx
@@ -0,0 +1,15 @@
+const WavesComponent = () => {
+  return (
+    <div className="border-b-2 pb-2">
+      <div>
+        <h1 className="font-bold"> Build House for Mummy </h1>
+        <p>Goal: N245,000,000</p>
+      </div>
+      <div>
+        <h1>Due: 23/15/2022</h1>
+      </div>
+    </div>
+  );
+};
+
+export default WavesComponent;
