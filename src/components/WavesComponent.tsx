0f2a88ab1068a1c4e807e83d10d020d4adf763ea
Katalyst33 <>
2022-03-12 01:26:22 +0000

    implemented conditional rendering

--- src/components/WavesComponent.tsx
+++ src/components/WavesComponent.tsx
@@ -5,6 +5,10 @@ import { WavesDataType } from "../../types/models";
 const WavesComponent = () => {
   const [waves, setWaves] = useState([] as WavesDataType[]);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
   const handleDelete = (index: any) => {
     const newWaves = waves.filter((wave, i) => i !== index);
     setWaves(newWaves);
@@ -12,17 +16,29 @@ const WavesComponent = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:5100/waves").then((res) => {
-      res.json().then((data) => {
-        console.log(data);
+    fetch("http://localhost:5100/waves")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not connect to server anymore");
+        }
+        return res.json();
+      })
+      .then((data) => {
         setWaves(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+        console.log(err.message);
       });
-    });
-    console.log("use effect ran", waves);
   }, []);
 
   return (
     <div className="">
+      {error && <div>{error}</div>}
+      {isLoading && <div>Is loading...</div>}
       {waves && (
         <WaveList waves={waves} title="All Waves" handleDelete={handleDelete} />
       )}
