7cd2812541adcc6ab53c384a648785bc1332835b
Katalyst33 <>
2022-03-12 00:59:31 +0000

    added json server

--- src/components/WavesComponent.tsx
+++ src/components/WavesComponent.tsx
@@ -3,38 +3,7 @@ import WaveList from "./WaveList";
 import { WavesDataType } from "../../types/models";
 
 const WavesComponent = () => {
-  const [waves, setWaves] = useState([
-    {
-      title: "Build House for mummy",
-      goal: "24,500,000,",
-      date: "23/11/2022",
-      id: 1,
-    },
-    {
-      title: "Buy Car for Papa",
-      goal: "12,000,000",
-      date: "23/10/2022",
-      id: 2,
-    },
-    {
-      title: "Estate Development",
-      goal: "30,000,000",
-      date: "01/11/2023",
-      id: 2,
-    },
-    {
-      title: "My wedding",
-      goal: "3,000,000",
-      date: "",
-      id: 1,
-    },
-    {
-      title: "Dubai trip with friends",
-      goal: "7,500,000",
-      date: "04/11/2022",
-      id: 1,
-    },
-  ] as WavesDataType[]);
+  const [waves, setWaves] = useState([] as WavesDataType[]);
 
   const handleDelete = (index: any) => {
     const newWaves = waves.filter((wave, i) => i !== index);
@@ -43,12 +12,20 @@ const WavesComponent = () => {
   };
 
   useEffect(() => {
+    fetch("http://localhost:5100/waves").then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        setWaves(data);
+      });
+    });
     console.log("use effect ran", waves);
-  }, [waves]);
+  }, []);
 
   return (
     <div className="">
-      <WaveList waves={waves} title="All Waves" handleDelete={handleDelete} />
+      {waves && (
+        <WaveList waves={waves} title="All Waves" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
