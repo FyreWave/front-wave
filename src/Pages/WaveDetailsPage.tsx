2226daed86426c582a38946457a0b7d9fc7193f1
Katalyst33 <>
2022-03-14 06:45:06 +0000

    add tab menu 30 %

--- src/Pages/WaveDetailsPage.tsx
+++ src/Pages/WaveDetailsPage.tsx
@@ -4,8 +4,61 @@ import useFetch from "../libs/useFetch";
 import WaveTile from "../components/waveComponents/WaveTile";
 import WaveUsers from "../components/waveComponents/WaveUsers";
 import WaveActivities from "../components/waveComponents/WaveActivities";
+import { useState } from "react";
+import TabView from "../components/waveComponents/TabView";
 
 const WaveDetailsPage = () => {
+  let [categories] = useState({
+    Recent: [
+      {
+        id: 1,
+        title: "Does drinking coffee make you smarter?",
+        date: "5h ago",
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: "2h ago",
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    Popular: [
+      {
+        id: 1,
+        title: "Is tech making coffee better or worse?",
+        date: "Jan 7",
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: "The most innovative things happening in coffee",
+        date: "Mar 19",
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+    Trending: [
+      {
+        id: 1,
+        title: "Ask Me Anything: 10 answers to your questions about coffee",
+        date: "2d ago",
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: "4d ago",
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+  });
+
   const params = useParams();
   const waveId = params.waveId;
   const history = useNavigate();
@@ -34,7 +87,7 @@ const WaveDetailsPage = () => {
         {isError && <div>Error</div>}
         {wave && (
           <div>
-            <div className="grid lg:grid-cols-2">
+            <div className="grid md:grid-cols-2">
               <div className="bg-white p-10">
                 <div className=" border-b-2">
                   <h4 className="text-secondary-500 text-3xl">
@@ -55,6 +108,10 @@ const WaveDetailsPage = () => {
             </div>
           </div>
         )}
+      </div>
+
+      <div className="flex justify-center md:hidden">
+        <TabView />
       </div>
 
       <div>
